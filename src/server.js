76b98da9653f244936340cc2693dcc5245112aef
10f7c7 <10f7c7@gmail.com>
2025-09-21 18:26:46 -0400

    removed tmp code

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ const port = 3000; // Or any desired port
 
 app.use('/', express.static("./build"));
 
-app.listen(port, "192.168.1.160", () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
